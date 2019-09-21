3ed48221dfe5297bebb73d8cf2033c82f0664718
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-09-22 00:06:31 +0700

    Naming Convention

---
+++ naming-convention.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var _deeptech string = "_deeptech"
+	fmt.Println(_deeptech)
+
+	var deep_tech = "deep_tech"
+	fmt.Println(deep_tech)
+
+	var dҼҼptech = "dҼҼptech"
+	fmt.Println(dҼҼptech)
+
+	var deeptech2019 = "deeptech2019"
+	fmt.Println(deeptech2019)
+
+	var namingConvention = "camelCase"
+	fmt.Println(namingConvention)
+}
---
+++ variable-declaration.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		firstName string = "Satoshi"
+		lastName  string = "Nakamoto"
+		age       int    = 35
+	)
+
+	fmt.Println(firstName)
+}
