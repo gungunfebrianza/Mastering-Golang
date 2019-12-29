58186dbf3e48e1aa34ffb3abebaa0b69c98322e6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-12-29 19:03:52 +0700

    Replace

--- src/2.String/strings.go
+++ src/2.String/strings.go
@@ -66,4 +66,8 @@ func main() {
 	//Repeat returns a new string consisting of count copies of the string s.
 	fmt.Println(strings.Repeat("Gun", 2)) // Output : GunGun
 
+	//Replace returns a copy of the string s with the first n non-overlapping instances of old replaced by new. If old is empty, it matches at the beginning of the string and after each UTF-8 sequence, yielding up to k+1 replacements for a k-rune string. If n < 0, there is no limit on the number of replacements.
+	fmt.Println(strings.Replace("oink oink oink", "k", "ky", 2))
+	fmt.Println(strings.Replace("oink oink oink", "oink", "moo", -1))
+
 }
