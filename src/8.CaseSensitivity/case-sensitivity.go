6779653a0c5a6dddd47b2a53edb958f06cba76e6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-12-30 03:05:20 +0700

    Case Sensitivity Testing

---
+++ src/8.CaseSensitivity/case-sensitivity.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A int // Case Sensitivity Test
+	var a int // Case Sensitivity Test
+	A = 10
+	a = 15
+	fmt.Println(A) // Output : 10
+	fmt.Println(a) // Output : 15
+}
