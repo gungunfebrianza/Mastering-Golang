02ec9581aca98edf706b0b5cd702e2f01c680bbb
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-12-29 18:20:10 +0700

    Compare

--- src/2.String/strings.go
+++ src/2.String/strings.go
@@ -13,5 +13,23 @@ func main() {
 	fmt.Println(strings.ToLower("Gun Gun Febrianza")) // Output : gun gun febrianza
 
 	//ToUpper returns s with all Unicode letters mapped to their upper case.
-	fmt.Println(strings.ToUpper("gun gun febrianza")) // Output : GUN GUN FEBRIANZA
+	fmt.Println(strings.ToUpper("gun gun febrianza")) // GUN GUN FEBRIANZA
+
+	//Contains reports whether substr is within s.
+	fmt.Println(strings.Contains("Grenade", "ena")) // Output : true
+	fmt.Println(strings.Contains("Grenade", "gun")) // Output : false
+	fmt.Println(strings.Contains("Gun", ""))        // Output : true
+	fmt.Println(strings.Contains("", ""))           // Output : true
+
+	//Compare returns an integer comparing two strings lexicographically. The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
+	fmt.Println(strings.Compare("a", "a"))         // Output : 0
+	fmt.Println(strings.Compare("a", "b"))         // Output : -1
+	fmt.Println(strings.Compare("b", "a"))         // Output : 1
+	fmt.Println(strings.Compare("5", "5"))         // Output : 0
+	fmt.Println(strings.Compare("5", "6"))         // Output : -1
+	fmt.Println(strings.Compare("6", "5"))         // Output : 1
+	fmt.Println(strings.Compare("makan", "makan")) // Output : 0
+	fmt.Println(strings.Compare("makan", "minum")) // Output : -1
+	fmt.Println(strings.Compare("minum", "makan")) // Output : 1
+
 }
