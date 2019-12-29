1e936e3cb7fde67deb5dcc8256055c001bd02316
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-12-29 11:12:29 +0700

    Update

--- src/1.Intro/HelloWorld.go
+++ src/1.Intro/HelloWorld.go
@@ -1,9 +1,9 @@
-package main 
- 
-import ( 
-    "fmt" 
-) 
- 
-func main() { 
-    fmt.Println("This is a sample Go program!") 
-} 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
