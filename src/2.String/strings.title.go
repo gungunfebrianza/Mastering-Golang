7aca10b8a10fc5d89ef50d7e54f5f5b4124db8be
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-12-29 17:57:34 +0700

    String Title

---
+++ src/2.String/strings.title.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Title("gun gun febrianza"))
+}
+
+// Title returns a copy of the string s with all Unicode letters that begin words mapped to their Unicode title case.
+// Output :
+//Gun Gun Febrianza
