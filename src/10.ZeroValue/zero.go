edcbf2559d49e861be269264bf81af972e152400
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-12-31 00:39:33 +0700

    Zero Value

---
+++ src/10.ZeroValue/zero.go
@@ -0,0 +1,14 @@
+	package main
+
+	import (
+		"fmt"
+	)
+
+	func main() {
+		var x int
+		var y string
+		var z bool
+		fmt.Println(x) // Output : 0
+		fmt.Println(y) // Output : ""
+		fmt.Println(z) // Output : false
+	}
