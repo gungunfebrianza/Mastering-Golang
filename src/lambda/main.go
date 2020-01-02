c03a3ebfe8fee7960ada0b3163f2cd48f0b09061
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-02 18:45:29 +0700

    Add New Function Lambda

--- src/lambda/main.go
+++ src/lambda/main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler() (events.APIGatewayProxyResponse, error) {
+var movies = []struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}{
+	{
+		ID:   1,
+		Name: "Avengers",
+	},
+	{
+		ID:   2,
+		Name: "Ant-Man",
+	},
+	{
+		ID:   3,
+		Name: "Thor",
+	},
+	{
+		ID:   4,
+		Name: "Hulk",
+	}, {
+		ID:   5,
+		Name: "Doctor Strange",
+	},
+}
+
+func findAll() (events.APIGatewayProxyResponse, error) {
+	response, err := json.Marshal(movies)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Serunya belajar serverless dengan golang!",
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(response),
 	}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(findAll)
 }
