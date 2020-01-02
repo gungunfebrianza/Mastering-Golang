4f027a5a3e830c2a51c7ec1c5d52e6d1edce3616
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-02 16:33:01 +0700

    Using API Gateway

--- src/lambda/main.go
+++ src/lambda/main.go
@@ -1,14 +1,12 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
-type Response struct {
-	StatusCode int    `json:"statusCode"`
-	Body       string `json:"body"`
-}
-
-func handler() (Response, error) {
-	return Response{
+func handler() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Serunya belajar serverless dengan golang!",
 	}, nil
