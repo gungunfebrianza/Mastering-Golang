dda4aa6c27c828640c4a5a74aa29fd62146348e5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-08 18:00:53 +0700

    Switch

---
+++ src/14.Logic/SwitchExpression/SwitchExpression.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	switch dayBorn := time.Sunday; {
+	case dayBorn == time.Sunday || dayBorn == time.Saturday:
+		fmt.Println("Born on the weekend")
+	default:
+		fmt.Println("Day, is not valid! Error!")
+	}
+}
