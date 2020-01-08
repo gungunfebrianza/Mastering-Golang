dc3b1f96d0fafe806d254a3c8a5de43a8e777d62
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-08 11:08:09 +0700

    Simplified

--- src/7.Variable/Pointer2/pointer.go
+++ src/7.Variable/Pointer2/pointer.go
@@ -12,8 +12,11 @@ func main() {
 	// Output : pointerInteger: 5
 	fmt.Printf("dataPointer: %#v\n", dataPointer)
 	// Output : dataPointer: (*int)(0xc0000140b0)
+
 	var pointerValue = dataPointer
 	fmt.Println(pointerValue)
+	// Output : 0xc0000140b0
+
 	if pointerValue != nil {
 		fmt.Printf("pointerValue: %#v\n", *pointerValue)
 		// Output : pointerValue: 5
