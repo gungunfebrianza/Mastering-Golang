ade82356cc216bdd219454f9bf0d42aa6b5f481d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-09 11:22:31 +0700

    Add

---
+++ src/3.String/StringRepeat/StringRepeat.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	//Repeat returns a new string consisting of count copies of the string s.
+	fmt.Println(strings.Repeat("Gun", 2))
+	// Output : GunGun
+}
