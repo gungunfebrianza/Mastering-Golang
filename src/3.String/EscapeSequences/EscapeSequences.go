c9046f9840eb15c46c7708a5d7c7851fed95fab6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-09 11:19:32 +0700

    Add

---
+++ src/3.String/EscapeSequences/EscapeSequences.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Escape Sequences
+	fmt.Println("\n Newline") // Add New Line
+	fmt.Println("\t tab")     // Add Tab
+	fmt.Println("\" \" ")     // Add Double Quote
+	fmt.Println(" \\ ")       // Add Backslash
+}
