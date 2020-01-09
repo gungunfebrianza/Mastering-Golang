25f801c29ae22f487e93b8398b15a51be30121d6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-09 11:24:08 +0700

    Rearrange

---
+++ src/5.Number/number.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(20)   // Integer
+	fmt.Println(3.14) // Floating-point number
+
+	fmt.Println(3 + 2)     // Return Integer
+	fmt.Println(3 - 2)     // Return Integer
+	fmt.Println(3 * 2)     // Return Integer
+	fmt.Println(3 / 2)     // Return Integer
+	fmt.Println(3 / 2.0)   // Return Floating-point
+	fmt.Println(3.0 / 2)   // Return Floating-point
+	fmt.Println(3.0 / 2.0) // Return Floating-point
+	fmt.Println(5.0 / 3.0) // Return Floating-point
+}
