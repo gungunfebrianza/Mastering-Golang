a19c77f252df2d232ddc540b03b64c7a8fa39ba8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-09 11:24:30 +0700

    Rearrange

---
+++ src/6.Boolean/boolean.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(7 > 2)  // Return True
+	fmt.Println(7 < 2)  // Return False
+	fmt.Println(8 == 8) // Return True
+	fmt.Println(8 == 2) // Return False
+	fmt.Println(8 != 2) // Return True
+	fmt.Println(3 <= 3) // Return True
+	fmt.Println(4 >= 4) // Return True
+}
