4dabd89fceeecd95282901595dd56df3dffd7a0e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-10 13:20:02 +0700

    Add

--- src/10.DataTypes/Floating-point/float-accuracy/FloatAccuracy.go
+++ src/10.DataTypes/Floating-point/float-accuracy/FloatAccuracy.go
@@ -1,12 +1,24 @@
-package main
+	package main
 
-import "fmt"
+	import (
+		"fmt"
+		"math"
+	)
 
-func main() {
-	var intVariable int = 100
-	var float32Variable float32 = 100
-	var float64Variable float64 = 100
-	fmt.Println(intVariable / 3)     // Output : 33
-	fmt.Println(float32Variable / 3) // Output : 33.333332
-	fmt.Println(float64Variable / 3) // Output : 33.333333333333336
-}
+	func main() {
+		var intVariable int = 100
+		var float32Variable float32 = 100
+		var float64Variable float64 = 100
+		fmt.Println(intVariable / 3)     // Output : 33
+		fmt.Println(float32Variable / 3) // Output : 33.333332
+		fmt.Println(float64Variable / 3) // Output : 33.333333333333336
+
+		var pi64 = math.Pi
+		var pi32 float32 = math.Pi
+		fmt.Println(pi64)
+		fmt.Println(pi32)
+		/*
+			3.141592653589793
+			3.1415927
+		*/
+	}
