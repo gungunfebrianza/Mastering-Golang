ae568ac3a35a3b8c27e565e3ce6f56582413adb4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-10 09:08:27 +0700

    add

--- src/10.DataTypes/Floating-point/floating-point.go
+++ src/10.DataTypes/Floating-point/floating-point.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var intVariable int = 100
-	var float32Variable float32 = 100
-	var float64Variable float64 = 100
-	fmt.Println(intVariable / 3)     // Output : 33
-	fmt.Println(float32Variable / 3) // Output : 33.333332
-	fmt.Println(float64Variable / 3) // Output : 33.333333333333336
+	var decimalPoint1 float32 = 1.23
+	fmt.Println(decimalPoint1) // Ouput : 1.23
+
+	var decimalPoint2 float32 = 01.23
+	fmt.Println(decimalPoint2) // Ouput : 1.23
+
+	var decimalPoint3 float32 = .23
+	fmt.Println(decimalPoint3) // Ouput : 0.23
+
+	var decimalPoint4 float32 = 2.
+	fmt.Println(decimalPoint4) // Ouput : 2
+
+	var exponentPart1 float32 = 1.88e2
+	fmt.Println(exponentPart1) // Output : 188
+
+	var exponentPart2 float32 = 188e2
+	fmt.Println(exponentPart2) // Output : 18800
+
+	var exponentPart3 float32 = 188e-2
+	fmt.Println(exponentPart3) // Output : 1.88
 }
