fafa4cb9c8a1fbb886393183334b4d857337a3da
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-12 23:27:31 +0700

    Add

---
+++ src/Array/DeclareArray/DeclareArray.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+
+	var names [3]string
+
+	names[0] = "Gun Gun Febrianza"
+	names[1] = "Nikolaj Vestorp"
+	names[2] = "Racheysa Hazna"
+
+	fmt.Println(names[0])
+	fmt.Println(names)
+	fmt.Println(reflect.TypeOf(names))
+
+	cars := [2]string{"Pajero","Lamborghini Veneno"}
+	fmt.Println(cars)
+}
