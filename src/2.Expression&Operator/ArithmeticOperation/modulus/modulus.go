5c5601ff29927eb43cf7a98a7ef3e4167954d50a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-04-07 23:16:26 +0700

    AddModulus

---
+++ src/2.Expression&Operator/ArithmeticOperation/modulus/modulus.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(12 % 5) //2
+	fmt.Println(-1 % 2); // -1
+  fmt.Println(1 % -2); // 1
+  fmt.Println(1 % 2); // 1
+  fmt.Println(2 % 3); // 2
+  fmt.Println(-4 % 2); // 0
+}
